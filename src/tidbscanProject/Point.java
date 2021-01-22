08cc2792357dc5e98ccc4d2405ff56d47697ef12
Micaela Pujol <mpujolh>
2021-01-22 11:49:39 +0100

    base

---
+++ src/tidbscanProject/Point.java
@@ -0,0 +1,16 @@
+package tidbscanProject;
+
+public class Point {
+
+	int[] coordinates;
+	String clusterID;	
+	double distance;
+	int neighborsNum;
+	Point[] border;
+	String[] metadata;
+	
+	public Point(Point point, Point referencePoint, String[] metadata) {
+		
+		
+	}
+}
--- src/tidbscanProject/tidbscan.java
+++ src/tidbscanProject/tidbscan.java
@@ -1,6 +1,6 @@
 package tidbscanProject;
 
-public class tidbscan {
+public class Tidbscan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
